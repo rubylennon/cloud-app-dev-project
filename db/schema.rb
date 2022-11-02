ee7ce7803f54d2380128c5a84b0fa5884698abfb
Ruby Lennon <x19128355@student.ncirl.ie>
2022-11-02 17:42:19 +0000

    added admin fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_161013) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_173523) do
   create_table "products", force: :cascade do |t|
     t.string "product_name"
     t.text "product_description"
@@ -27,6 +27,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_161013) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
