78d8193c8d0f569d45587b1be3cce23d1ef509f1
Ruby Lennon <x19128355@student.ncirl.ie>
2023-01-13 20:50:22 +0000

    fix test issue

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,5 +3,15 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    sign_in users(:admin)
+    get categories_url
+    assert_response :success
+  end
 
+  test 'should get new if user admin' do
+    sign_in users(:admin)
+    get new_category_url
+    assert_response :redirect
+  end
 end
